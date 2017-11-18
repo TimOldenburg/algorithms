ebd57c7352432aaf6022ad24210dd9f36dcad6d6
Tim Oldenburg <TimOld.Tr@gmail.com>
2017-11-18 13:22:42 +0100

    Added basic algorithm challenge

---
+++ 27 - Missing letters.js
@@ -0,0 +1,20 @@
+/*
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+*/
+
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.charCodeAt(i) !== str.charCodeAt(i + 1) - 1) {
+      return String.fromCharCode(str.charCodeAt(i) + 1);
+    }
+  }
+  return undefined;
+}
+
+console.log(fearNotLetter("abce")) // should return "d".
+console.log(fearNotLetter("abcdefghjklmno")) // should return "i".
+console.log(fearNotLetter("stvwx")) // should return "u".
+console.log(fearNotLetter("bcdf")) // should return "e".
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz")) // should return undefined.
