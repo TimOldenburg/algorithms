c49778e1803a31f1cc51ce6820209dbf2ea226fc
Tim Oldenburg <TimOld.Tr@gmail.com>
2017-11-21 12:22:17 +0100

    Added basic algorithm challenge

---
+++ 29 - Convert HTML Entities.js
@@ -0,0 +1,22 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+  let obj = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  return str.replace(/&|>|<|"|'/g, a => obj[a]);
+}
+
+console.log(convertHTML("Dolce & Gabbana")) // should return Dolce &​amp; Gabbana.
+console.log(convertHTML("Hamburgers < Pizza < Tacos")) // should return Hamburgers &​lt; Pizza &​lt; Tacos.
+console.log(convertHTML("Sixty > twelve")) // should return Sixty &​gt; twelve.
+console.log(convertHTML('Stuff in "quotation marks"')) // should return Stuff in &​quot;quotation marks&​quot;.
+console.log(convertHTML("Schindler's List")) // should return Schindler&​apos;s List.
+console.log(convertHTML("<>")) // should return &​lt;&​gt;.
+console.log(convertHTML("abc")) // should return abc.
